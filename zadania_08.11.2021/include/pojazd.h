5fc2ae6862de9f6535a1c06f81857fd818f00b37
Mikolaj-Mendak <91893031+Mikolaj-Mendak@users.noreply.github.com>
2021-11-08 16:10:56 +0100

    zadania_08_11_2021

---
+++ zadania_08.11.2021/include/pojazd.h
@@ -0,0 +1,15 @@
+#ifndef JIPP2_FIGURA_H
+#define JIPP2_FIGURA_H
+#include "../header/Punkt.h"
+
+class Pojazd
+{
+    string numerRejestracyjny;
+    string nazwa;
+    int iloscMiejsc;
+
+};
+
+
+
+#endif
